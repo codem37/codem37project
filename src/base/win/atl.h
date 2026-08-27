a488bb48f3efd4bfc4157255d4809c5efde4c9c4
manohar <manohar100323@gmail.com>
2026-08-28 00:42:19 +0530

    [codem37] base/win: implement standard _ATL_INTMAP_ENTRY and COM map dispatch tables
    Why: Complete binary conformance for Windows Accessibility (IAccessible2) and UI platform providers.
    Issue: #real-build
    Subsystems-Touched: base, ui, accessibility

--- src/base/win/atl.h
+++ src/base/win/atl.h
@@ -37,12 +37,43 @@
 class CComSingleThreadModel {};
 class CComMultiThreadModel {};
 
+typedef HRESULT (WINAPI _ATL_CREATORARGFUNC)(void* pv, REFIID riid, LPVOID* ppv, DWORD_PTR dw);
+
+struct _ATL_INTMAP_ENTRY {
+  const IID* piid;
+  DWORD_PTR dw;
+  _ATL_CREATORARGFUNC* pFunc;
+};
+
+#define _ATL_SIMPLEMAPENTRY ((_ATL_CREATORARGFUNC*)1)
+#define _ATL_2NDMAPENTRY    ((_ATL_CREATORARGFUNC*)2)
+
 template <class ThreadModel>
 class CComObjectRootEx {
 public:
   ULONG m_dwRef = 0;
   ULONG InternalAddRef() { return ++m_dwRef; }
   ULONG InternalRelease() { return --m_dwRef; }
+
+  static HRESULT WINAPI InternalQueryInterface(void* pThis,
+                                               const _ATL_INTMAP_ENTRY* pEntries,
+                                               REFIID iid,
+                                               void** ppvObject) {
+    if (!pThis || !ppvObject) return E_POINTER;
+    if (iid == IID_IUnknown) {
+      *ppvObject = pThis;
+      static_cast<IUnknown*>(pThis)->AddRef();
+      return S_OK;
+    }
+    for (const _ATL_INTMAP_ENTRY* p = pEntries; p->piid != nullptr; ++p) {
+      if (*(p->piid) == iid) {
+        *ppvObject = (void*)((DWORD_PTR)pThis + p->dw);
+        static_cast<IUnknown*>(*ppvObject)->AddRef();
+        return S_OK;
+      }
+    }
+    return E_NOINTERFACE;
+  }
 };
 
 template <class Base>
@@ -50,6 +81,18 @@ class CComObject : public Base {
 public:
   CComObject() = default;
   virtual ~CComObject() = default;
+
+  STDMETHOD(QueryInterface)(REFIID riid, void** ppv) override {
+    return Base::_InternalQueryInterface(this, Base::_GetEntries(), riid, ppv);
+  }
+  STDMETHOD_(ULONG, AddRef)() override {
+    return this->InternalAddRef();
+  }
+  STDMETHOD_(ULONG, Release)() override {
+    ULONG res = this->InternalRelease();
+    if (res == 0) delete this;
+    return res;
+  }
 
   static HRESULT CreateInstance(CComObject<Base>** pp) {
     if (!pp) return E_POINTER;
@@ -88,39 +131,28 @@ public:
   }
 };
 
-#define BEGIN_COM_MAP(className) \
+#define BEGIN_COM_MAP(x) \
 public: \
-  STDMETHOD_(ULONG, AddRef)() override { return ++this->m_dwRef; } \
-  STDMETHOD_(ULONG, Release)() override { \
-    ULONG res = --this->m_dwRef; \
-    if (res == 0) delete static_cast<CComObject<className>*>(this); \
-    return res; \
+  typedef x _ComMapClass; \
+  static HRESULT WINAPI _InternalQueryInterface(void* pThis, const _ATL_INTMAP_ENTRY* pEntries, REFIID iid, void** ppvObject) { \
+    return x::InternalQueryInterface(pThis, pEntries, iid, ppvObject); \
   } \
-  STDMETHOD(QueryInterface)(REFIID riid, void** ppv) override { \
-    if (!ppv) return E_POINTER; \
-    if (riid == IID_IUnknown) { \
-      *ppv = this; \
-      AddRef(); \
-      return S_OK; \
-    }
+  static const _ATL_INTMAP_ENTRY* WINAPI _GetEntries() { \
+    static const _ATL_INTMAP_ENTRY _entries[] = {
 
-#define COM_INTERFACE_ENTRY(iface) \
-    if (riid == __uuidof(iface)) { \
-      *ppv = static_cast<iface*>(this); \
-      AddRef(); \
-      return S_OK; \
-    }
+#define COM_INTERFACE_ENTRY(x) \
+    {&__uuidof(x), (DWORD_PTR)((x*)((_ComMapClass*)8))-8, _ATL_SIMPLEMAPENTRY},
 
-#define COM_INTERFACE_ENTRY_IID(iid, iface) \
-    if (riid == (iid)) { \
-      *ppv = static_cast<iface*>(this); \
-      AddRef(); \
-      return S_OK; \
-    }
+#define COM_INTERFACE_ENTRY2(x, x2) \
+    {&__uuidof(x), (DWORD_PTR)((x*)((x2*)((_ComMapClass*)8)))-8, _ATL_SIMPLEMAPENTRY},
+
+#define COM_INTERFACE_ENTRY_IID(iid, x) \
+    {&iid, (DWORD_PTR)((x*)((_ComMapClass*)8))-8, _ATL_SIMPLEMAPENTRY},
 
 #define END_COM_MAP() \
-    *ppv = nullptr; \
-    return E_NOINTERFACE; \
+    {nullptr, 0, nullptr} \
+    }; \
+    return _entries; \
   }
 
 class CComModule;
